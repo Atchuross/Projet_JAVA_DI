3d1ff3ee44513dd77749255cec116e06681928dd
Atchuross <clement.czl9002@gmail.com>
2021-01-18 11:11:36 +0100

    Ajout Class Organisation

---
+++ src/Organisation.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Organisation {
+    public String nom;
+    public String siegeSocial;
+    public String dirigeant;
+    private ArrayList<String> membres = new ArrayList<>();
+    private String commentaire;
+    private String dateAjout;
+    private String dateLastModif;
+    private int incidentReported;
+    private int envlovedIn;
+}
