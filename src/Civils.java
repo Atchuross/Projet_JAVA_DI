072d40872a870ecfc9137b937f1817deac65d534
Atchuross <clement.czl9002@gmail.com>
2021-01-18 11:00:23 +0100

    Ajout Class Civils

---
+++ src/Civils.java
@@ -0,0 +1,15 @@
+public class Civils {
+    private String nom;
+    private String prenom;
+    private String civilite;
+    private String email;
+    private String telephone;
+    private String dateDeNaissance;
+    private String dateDeDeces;
+    public String organisation;
+    private String commentaire;
+    private String dateAjout;
+    private String dateModif;
+    private int incidentReported;
+    private int envlovedIn;
+}
