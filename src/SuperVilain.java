50219773ecc145f663b55e126d9e33363081dfda
Atchuross <clement.czl9002@gmail.com>
2021-01-18 11:31:01 +0100

    Ajout Class SuperVilain

---
+++ src/SuperVilain.java
@@ -0,0 +1,8 @@
+public class SuperVilain {
+    public String nom;
+    private String secretId;
+    public String pouvoir;
+    private String pointFaible;
+    private int degats;
+    private int nmbrMefait;
+}
