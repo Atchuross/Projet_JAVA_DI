e0edf92ba24d90e81a3cf569fb224325661ff79a
Atchuross <clement.czl9002@gmail.com>
2021-01-18 14:49:40 +0100

    Finalisation Incidents

--- src/Incidents.java
+++ src/Incidents.java
@@ -10,10 +10,25 @@ public class Incidents extends Civils{
 
         System.out.println("Saisissez le lieux de l'incidents : ");
         this.lieux = scanner.nextLine();
-        System.out.println("Saisissez votre nom : ");
-        super.nom = scanner.nextLine();
-        System.out.println("Saisissez votre prénom : ");
-        super.prenom = scanner.nextLine();
+        System.out.println("Saisissez le type de l'incidents : ");
+        this.typeIncident = scanner.nextLine();
+        System.out.println("Un Super-Vilain est-il présent sur place ? 1/2");
+        int yon = scanner.nextInt();
+        if(yon == 1){
+            System.out.println("Lequel ? : ");
+            this.mechantSurPlace = scanner.nextLine();
+        }else if(yon == 2){
+            System.out.println("Très bien");
+            System.out.println("Saisissez votre nom : ");
+            super.nom = scanner.nextLine();
+            System.out.println("Saisissez votre prénom : ");
+            super.prenom = scanner.nextLine();
+            System.out.println("Email : ");
+            super.email = scanner.nextLine();
+            System.out.println("Telephone : ");
+            super.telephone = scanner.nextLine();
+        }
+
     }
 
     public void getIncident(){
