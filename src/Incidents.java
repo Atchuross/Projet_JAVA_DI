e400357a3f4dbf4a9e74ad377abfefa2c53d694a
Atchuross <clement.czl9002@gmail.com>
2021-01-18 13:48:52 +0100

    Build incident section

--- src/Incidents.java
+++ src/Incidents.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Incidents extends Civils{
     public String lieux;
     public String mechantSurPlace;
     public String typeIncident;
+
+    public void setIncident() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Saisissez le lieux de l'incidents : ");
+        this.lieux = scanner.nextLine();
+        System.out.println("Saisissez votre nom : ");
+        super.nom = scanner.nextLine();
+        System.out.println("Saisissez votre prénom : ");
+        super.prenom = scanner.nextLine();
+    }
+
+    public void getIncident(){
+        System.out.println(this.lieux);
+        System.out.println(super.nom);
+        System.out.println(super.prenom);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        Civils civil1 = new Civils();
+        Incidents incident1 = new Incidents();
+
+        incident1.setIncident();
+        incident1.getIncident();
     }
 }
