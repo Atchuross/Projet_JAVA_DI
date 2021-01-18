62e9bb033e61852dc994158ba3243a6ee0807723
Atchuross <clement.czl9002@gmail.com>
2021-01-18 11:53:25 +0100

    Ajout class Mission

---
+++ src/Mission.java
@@ -0,0 +1,9 @@
+public class Mission {
+    private String nature;
+    private String titre;
+    public String dateDebut;
+    public String dateFin;
+    public String Intinéraire;
+    private String infoComp;
+    private String NiveauGravite = "Normal";
+}
