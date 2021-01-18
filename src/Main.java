87bdf3c6b2f1f8d4703783d423f871d81169536d
Atchuross <clement.czl9002@gmail.com>
2021-01-18 14:02:06 +0100

    Ajout d'un menu command line

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Incidents incident1 = new Incidents();
+        Scanner menu = new Scanner(System.in);
 
-        incident1.setIncident();
-        incident1.getIncident();
+        System.out.println("#####################");
+        System.out.println("######SUPER_CRM######");
+        System.out.println("#####################");
+
+        System.out.println(" ");
+
+        System.out.println("1. Déclarer un Incidents");
+        System.out.println("2. Voir les missions");
+        int choix = menu.nextInt();
+
+
+        System.out.println(choix);
+
+        if(choix == 1){
+            System.out.println("Veuillez renter les informations suivante");
+            incident1.setIncident();
+            incident1.getIncident();
+        }else if(choix == 2){
+            System.out.println("Aucune mission en cours...");
+        }
+
+
     }
 }
