075f014a7d3a3cbdc4b6a5007f460b7611180198
Atchuross <57393426+Atchuross@users.noreply.github.com>
2021-04-08 20:20:23 +0200

    🛠 Ajout profil perso a la connexion

---
+++ Old Class/SuperHero.java
@@ -0,0 +1,8 @@
+public class SuperHero {
+    public String nom;
+    private String secretId;
+    public String pouvoir;
+    private String pointFaible;
+    private int Score;
+    private String commentaire;
+}
